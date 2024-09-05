44a35f528d047a5e6e4acad94b198e4559cde4f7
Dessimal <you@example.com>
2024-09-05 11:45:24 +0100

    edited step4 with chatgpts help to make it better

--- src/components/pages/ReservationForms/Step4.jsx
+++ src/components/pages/ReservationForms/Step4.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { calculateAmount } from "../../../helpers/calculation";
 import Heading from "../../Heading";
 import Button from "../../Button";
 import { FaArrowLeft, FaLocationArrow } from "react-icons/fa";
+import { calculateAmount } from "../../../helpers/calculation";
 
 const Step4 = ({ reservation, handlePrevious, handleProceedToPayment }) => {
+  const amountToPay = calculateAmount(reservation.diners);
+
   return (
     <section className="section-container">
       <Heading>Your Payment Details</Heading>
 
       <p>
-        <strong>Number of diners selected:</strong>
-        {reservation.diners}
+        <strong>Number of diners selected:</strong> {reservation.diners}
       </p>
       <p>
-        <strong>Amount per diner:</strong>N5000
+        <strong>Amount per diner:</strong> N5000
       </p>
       <p>
-        <strong>Amount to pay:</strong>
-        {calculateAmount(reservation.diners)}
+        <strong>Amount to pay:</strong> N{amountToPay}
       </p>
 
       <div className="nav-buttons-container">
@@ -26,7 +26,7 @@ const Step4 = ({ reservation, handlePrevious, handleProceedToPayment }) => {
           <FaArrowLeft className="icon-left" />
           <span>Go Back</span>
         </Button>
-        <Button onClick={() => handleProceedToPayment()}>
+        <Button onClick={handleProceedToPayment}>
           <span>Proceed with payment</span>
           <FaLocationArrow className="icon" />
         </Button>
